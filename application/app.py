89020faf73845ac6104e6b701efa141aba9aaba6
zaafir89 <jb.hallassou@gmail.com>
2025-05-26 15:46:26 +0200

    modif arbo streamlit

---
+++ application/app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas
+
+st.set_page_config(
+   # page_title="Mon Application à 5 Pages",
+   # page_icon="📘",
+    #layout="wide",
+    #initial_sidebar_state="expanded"
+)
+
+st.title("Bienvenue sur mon application Streamlit")
+st.write("Utilisez le menu à gauche pour naviguer entre les pages.")
+
+
+
---
+++ application/pages/search.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("Recherche de films")
+st.write("Ceci est la page de recherche de films.")
