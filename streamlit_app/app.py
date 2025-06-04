ae24ad1a47d604c93eba60544127a35a31297718
jbhdev <jb.hallassou@gmail.com>
2025-06-04 17:46:05 +0200

    petite modif

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import os
 from home import home_page
 from search import search_page
+from mylist import my_list_page
 from movie_detail import movie_detail_page, show_movie_details
 from utils.css_loader import load_css
 
@@ -46,17 +47,13 @@ def main():
             st.session_state.current_page = 'search'
             st.query_params.clear()
         if st.button("MA LISTE", key="nav_ma_liste_sidebar"):
-            st.session_state.current_page = 'home'
-            st.info("La page 'MA LISTE' n'est pas encore implémentée.")
+            st.session_state.current_page = 'my_list' # <--- CHANGE THIS TO 'my_list'
+            st.query_params.clear()
         if st.button("FILMS", key="nav_films_sidebar"):
             st.session_state.current_page = 'movie'
             st.query_params.clear()
 
-        st.markdown('<div class="sidebar-profile-container">', unsafe_allow_html=True)
-        user_pic_url = st.session_state.get("user_pic_url", "https://placehold.co/60x60/663399/ffffff?text=User")
-        st.image(user_pic_url, width=60, clamp=True)
-        st.markdown("<p class='sidebar-profile-name'>Johnbie</p>", unsafe_allow_html=True)
-        st.markdown('</div>', unsafe_allow_html=True)
+        
 
     # --- Main content ---
     if st.session_state.current_page == 'search':
